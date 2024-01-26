14ec929500650bf6abd62ca2dfd73c4dedcef0dc
Narendra-Rajput003 <narendrarajput05007@gmail.com>
2024-01-26 23:27:36 +0530

    change the code

--- index.js
+++ index.js
@@ -6,33 +6,41 @@ const socketio = require('socket.io');
 const io = socketio(server);
 const connect = require('./config/database');
 const chat = require('./models/chat');
+const cron = require('node-cron');
+const path = require('path');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-
 
 
 io.on('connection', (socket) => {
 
     socket.on('join_room', (data) => {
         socket.join(data.roomid);
-        console.log("joined room", data.roomid);
-
     })
 
+   
+    
     socket.on('msg_send', (data) => {
         const messageData = {
             msg: data.msg,
             inputUsername: data.inputUsername,
             roomid: data.roomid,
-            timestamp: new Date().toISOString() // Ensure a valid timestamp is sent
+            timestamp: new Date().toISOString() 
         };
+        // io.to method is used to send message to a specific room 
         io.to(data.roomid).emit('msg_rvcd', messageData);
     });
 
 
 
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 app.set('view engine', 'ejs');
 
 
